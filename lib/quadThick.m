af7ecfcdb4c6ab772024cb7d70b24b2dc1111c68
Kevin Mattheus Moerman <Kevin-Mattheus-Moerman@users.noreply.github.com>
2017-02-27 13:11:07 -0500

    Updated quadThick.m to allow user to specify direction of thickening

--- lib/quadThick.m
+++ lib/quadThick.m
@@ -1,26 +1,67 @@
-function [varargout]=quadThick(Fq,Vq,dirFlip,elementThickness,ns)
+function [varargout]=quadThick(varargin)
+
 
 %%
-%Get vertex normals
-[~,~,Nv]=patchNormal(Fq,Vq);
 
-%Flip if desired
-Nv=Nv.*dirFlip;
+switch nargin
+    case 2
+        Fq=varargin{1};
+        Vq=varargin{2};
+        dirSet=1;
+        layerThickness=mean(patchEdgeLengths(Fq,Vq));
+        numSteps=1;
+    case 3
+        Fq=varargin{1};
+        Vq=varargin{2};
+        dirSet=varargin{3};
+        layerThickness=mean(patchEdgeLengths(Fq,Vq));
+        numSteps=1;
+    case 4
+        Fq=varargin{1};
+        Vq=varargin{2};
+        dirSet=varargin{3};
+        layerThickness=varargin{4};
+        numSteps=1;
+    case 5
+        Fq=varargin{1};
+        Vq=varargin{2};
+        dirSet=varargin{3};
+        layerThickness=varargin{4};
+        numSteps=varargin{5};
+end
+
+%%
+
+if size(dirSet,2)==3 %single vector or vector set for offsetting
+    if size(dirSet,1)==1 %If one vector is given copy for all points
+        Nv=dirSet(ones(size(Vq,1),1),:);
+    else %assume vectors are provided for all points
+        if size(dirSet,1)~=size(Vq,1)
+            error('Number of normals should match number of points');
+        end
+    end
+else
+    %Get vertex normals
+    [~,~,Nv]=patchNormal(Fq,Vq);
+    
+    %Flip if desired
+    Nv=Nv.*dirSet;
+end
 
 %Thicken inwards
-Vq2=Vq+elementThickness.*Nv;
+Vq2=Vq+layerThickness.*Nv;
 
 %Get coordinates
-X=linspacen(Vq(:,1),Vq2(:,1),ns+1);
-Y=linspacen(Vq(:,2),Vq2(:,2),ns+1);
-Z=linspacen(Vq(:,3),Vq2(:,3),ns+1);
+X=linspacen(Vq(:,1),Vq2(:,1),numSteps+1);
+Y=linspacen(Vq(:,2),Vq2(:,2),numSteps+1);
+Z=linspacen(Vq(:,3),Vq2(:,3),numSteps+1);
 
 %Collect node set
 V=[X(:) Y(:) Z(:)];
 
 %Create element matrix
-E=repmat(Fq,[ns,2]);
-E_add=0:size(Vq,1):size(Vq,1)*(ns-1); 
+E=repmat(Fq,[numSteps,2]);
+E_add=0:size(Vq,1):size(Vq,1)*(numSteps-1); 
 E_add=E_add(ones(size(Fq,1),1),:);
 E_add=E_add(:);
 E_add=E_add(:,ones(4,1));
