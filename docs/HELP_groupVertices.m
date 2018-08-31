b551b9c8b58c5ddf1d3609f0663fbb0bfa2495d4
Kevin-Mattheus-Moerman <kevin.moerman@gmail.com>
2018-08-31 14:49:19 +0100

    Added new groupVertices function for grouping patch and element data

---
+++ docs/HELP_groupVertices.m
@@ -0,0 +1,112 @@
+%% groupVertices
+% Below is a demonstration of the features of the |groupVertices| function
+
+%%
+clear; close all; clc;
+
+%% Syntax
+% |[groupIndexVertices,groupIndexFaces]=groupVertices(F,V,waitBarOption);|
+
+%% Description
+% This function takes in a description of a tesselation, consisting of an
+% array F (faces or elements), and V (vertices). The output is represents
+% the group index for each point in groupIndexVertices and the group index
+% of each face in groupIndexFaces. Vertices which are connected to the same
+% mesh obtain the same group indes. Face indices are derived from the
+% vertex indices. 
+
+%% Examples
+
+%% Example 1: Seperating patch data into groups
+
+%%
+% Creating test data consisting of seperates sets faces and vertices
+
+numGroups=3; 
+F_cell=cell(1,numGroups); 
+V_cell=cell(1,numGroups); 
+for q=1:1:numGroups
+    switch q
+        case 1
+            [F,V]=geoSphere(3,1);
+        case 2
+            [F,V]=stanford_bunny('g');
+        case 3
+            [F,V]=graphicsModels(3);
+    end
+%     [F,V]=subtri(F,V,1);
+    V=V-mean(V,1);
+    V=V./max(V(:));
+    V(:,1)=V(:,1)+q*2;
+    F_cell{q}=F;
+    V_cell{q}=V;
+end
+[F,V]=joinElementSets(F_cell,V_cell);
+
+%%
+% Using |groupVertices| to split the vertices into groups
+[groupIndexVertices,groupIndexFaces]=groupVertices(F,V,1);
+
+%%
+% Visualizing the resulting grouping
+
+cFigure; 
+subplot(2,1,1); hold on;
+title('Ungrouped')
+gpatch(F,V,'kw','none');
+axisGeom;
+camlight headlight; 
+
+subplot(2,1,2); hold on;
+title('Grouped')
+gpatch(F,V,'kw','none');
+scatterV(V,15,groupIndexVertices,'filled');
+axisGeom;
+camlight headlight; 
+colormap gjet; icolorbar;
+drawnow;
+
+%%
+% If desired a second output can be requisted which represents the face
+% groupings. 
+
+cFigure; 
+title('Grouped faces')
+gpatch(F,V,groupIndexFaces,'none');
+axisGeom;
+camlight headlight; 
+colormap gjet; icolorbar;
+drawnow;
+
+%% 
+%
+% <<gibbVerySmall.gif>>
+% 
+% _*GIBBON*_ 
+% <www.gibboncode.org>
+% 
+% _Kevin Mattheus Moerman_, <gibbon.toolbox@gmail.com>
+ 
+%% 
+% _*GIBBON footer text*_ 
+% 
+% License: <https://github.com/gibbonCode/GIBBON/blob/master/LICENSE>
+% 
+% GIBBON: The Geometry and Image-based Bioengineering add-On. A toolbox for
+% image segmentation, image-based modeling, meshing, and finite element
+% analysis.
+% 
+% Copyright (C) 2018  Kevin Mattheus Moerman
+% 
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
---
+++ lib/groupVertices.m
@@ -0,0 +1,87 @@
+function [varargout]=groupVertices(varargin)
+
+% function [groupIndexVertices,groupIndexFaces]=groupVertices(F,V,waitBarOption)
+%-------------------------------------------------------------------------
+%
+%
+%
+% Change log: 
+% 2018/08/31 Created
+%
+%-------------------------------------------------------------------------
+%% Parse input
+
+switch nargin
+    case 2
+        F=varargin{1};
+        V=varargin{2};
+        waitBarOption=0;
+    case 3
+        F=varargin{1};
+        V=varargin{2};
+        waitBarOption=varargin{3};
+end
+waitBarOption=waitBarOption>0;
+
+%% Get vertex-vertex connectivity
+
+% C=patchConnectivity(F,V);
+% vertexVertexConnectivity=C.vertex.vertex;
+numVertices=size(V,1);
+E=patchEdges(F,0); %The non-unique edge set
+E_sort=sort(E,2); %Sorted in column dir so 1 2 looks the same as 2 1
+indEdges=sub2indn(numVertices*ones(1,2),E_sort); %Create "virtual" indices
+[~,ind1,~]=unique(indEdges); %Get indices for unique edges
+E_uni=E(ind1,:); %Get unique edges
+EV=[E_uni;fliplr(E_uni)];
+vertexVertexConnectivity=sparse(EV(:,1),EV(:,2),EV(:,2),numVertices,numVertices);
+vertexVertexConnectivity=sort(vertexVertexConnectivity,2,'descend');
+[~,J,~] = find(vertexVertexConnectivity);
+vertexVertexConnectivity=full(vertexVertexConnectivity(:,1:max(J)));
+
+%% Grouping vertices
+
+if waitBarOption==1
+    hw = waitbar(0,'Grouping vertices...');
+end
+
+groupIndexVertices=zeros(size(V,1),1);
+groupCounter=1; %The group counter
+allDone=0;
+while allDone==0
+    groupFinished=0;
+    vertexIndicesGroup=find(groupIndexVertices==0,1); %Initialize group
+    numGroupPrevious=[]; %Initialize empty group size
+    while groupFinished==0
+        %Grow group
+        vertexIndicesGroup=vertexVertexConnectivity(vertexIndicesGroup,:);
+        vertexIndicesGroup=vertexIndicesGroup(vertexIndicesGroup>0);
+        vertexIndicesGroup=unique(vertexIndicesGroup);
+        numGroupNow=numel(vertexIndicesGroup); %Get current group size
+        if numGroupNow==numGroupPrevious
+            groupFinished=1;
+        end
+        numGroupPrevious=numGroupNow; %Store previous group size
+        if waitBarOption==1
+            groupIndexVertices(vertexIndicesGroup)=groupCounter;
+            waitbar(nnz(groupIndexVertices>0)/numVertices,hw);
+        end
+    end
+    if waitBarOption~=1
+        groupIndexVertices(vertexIndicesGroup)=groupCounter;
+    end
+    groupCounter=groupCounter+1;
+    if nnz(groupIndexVertices>0)==numVertices
+        allDone=1;
+    end
+end
+
+if waitBarOption==1
+    close(hw)
+end
+
+%% Collect output
+varargout{1}=groupIndexVertices;
+if nargout==2
+    varargout{2}=vertexToFaceMeasure(F,groupIndexVertices);
+end
