8a28f14177514a7987e92414965914866bcdbdbb
Kevin-Mattheus-Moerman <kevin.moerman@gmail.com>
2020-06-24 21:31:31 +0100

    Added handling of NaN by cmaperise (forced to zero)

--- lib/cmaperise.m
+++ lib/cmaperise.m
@@ -41,9 +41,13 @@ end
 %% Use colormap to define RGB values
 
 C=C(:); %Color data as column
+
 p=(C-clim(1))./(clim(2)-clim(1));
 p(p<0)=0;
 p(p>1)=1;
+
+p(isnan(p))=0; %Force nan data to 0
+
 IND_cmap=round((p*(size(cmap,1)-1))+1);
 Cmapped=cmap(IND_cmap,:);
 
