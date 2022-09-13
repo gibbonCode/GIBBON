7ad19bee493823d93d162263d2a71d470cb79932
Kevin-Mattheus-Moerman <kevin.moerman@gmail.com>
2022-09-13 16:37:39 +0100

    Added gedge for visualizing edge data

---
+++ docs/HELP_gedge.m
@@ -0,0 +1,172 @@
+%% gedge
+% Below is a demonstration of the features of the |dihedralAngles| function
+
+%%
+clear; close all; clc;
+
+%% Syntax
+% |[h]=gedge(E,V,AE,5,AE);|
+
+%% Description 
+% The |gedge| function aids in the visualization of edges e.g. of meshes.
+% Edge coloring and transparency can be prescribed. 
+% 
+% SEe also |gpatch|. 
+
+%% Examples 
+% 
+
+%%
+% Plot settings
+fontSize=20;
+edgeThickness=5; 
+cmap=gjet(250);
+
+%%
+% Create example data, mesh data for a hex mesh
+
+%Create mesh for a cube
+boxDim=2*ones(1,3);
+boxEl=1*ones(1,3);
+[meshStruct]=hexMeshBox(boxDim,boxEl);
+Eh=meshStruct.E;
+V=meshStruct.V;
+F=meshStruct.F; 
+
+%Distorting the shape (shear)
+d=eye(3,3); %Identity
+d(1,2)=1; %Add shear
+V=V*d; %Deform
+
+%Get mesh edges
+E=patchEdges(F,V); 
+
+%% Example 1: Visualizing edges in a single color
+
+cFigure; 
+subplot(1,2,1); hold on;
+title('gpatch visualization of mesh faces and edges'); 
+gpatch(F,V,'rw','r',1,edgeThickness);
+axisGeom; camlight headlight; 
+
+subplot(1,2,2); hold on;
+title('gedge visualization of mesh edges only'); 
+gedge(E,V,'r',edgeThickness);
+axisGeom; camlight headlight; 
+
+drawnow; 
+
+%% Example 2: Using colormapping on the edges
+
+%%
+% Create example color data on edges, e.g. the mesh dihedral angle
+
+[~,E,AE]=dihedralAngles(Eh,V,'hex8');
+AE=180*(AE./pi);
+
+%%
+
+cFigure; hold on;
+title('Color data on edges')
+gedge(E,V,AE,edgeThickness);
+axisGeom; camlight headlight; 
+colormap(cmap); colorbar; 
+gdrawnow; 
+
+%% Example 3: Specifying edge transparency as well
+
+cFigure; hold on;
+title('Alpha data and color data on edges')
+gedge(E,V,AE,edgeThickness,AE);
+axisGeom; camlight headlight; 
+colormap(cmap); colorbar; 
+alim([25 135]);
+gdrawnow; 
+
+%% Example 4: Interpolated colors from vertex (node) data
+
+[F,V]=geoSphere(1,1);
+E=patchEdges(F);
+VE=patchCentre(E,V);
+CE=VE(:,1); %Example edge color data  = mid-edge x-coordinate
+CV=V(:,1); %Example vertex color data = x-coordinate
+
+%%
+
+cFigure; hold on;
+title('Color data on vertices')
+h= gedge(E,V,CV,edgeThickness);
+h.EdgeColor='interp'; %Turn on interpolated shading
+axisGeom; camlight headlight; 
+colormap(cmap); colorbar; 
+gdrawnow; 
+
+%% Example 5: Specifying RGB color data on edges
+
+C_rgb1=cmaperise(CE,gjet(250));
+C_rgb2=cmaperise(CE,viridis(250));
+C_rgb3=cmaperise(CE,spectral(250));
+
+V2=V;
+V2(:,1)=V2(:,1)+2.5; 
+V3=V2;
+V3(:,1)=V3(:,1)+2.5; 
+
+%%
+
+cFigure; hold on;
+title('RGB data on edges')
+h1= gedge(E,V ,C_rgb1,edgeThickness);
+h2= gedge(E,V2,C_rgb2,edgeThickness);
+h3= gedge(E,V3,C_rgb3,edgeThickness);
+axisGeom; camlight headlight; 
+gdrawnow; 
+
+%% Example 6: Specifying RGB color data on vertices (nodes)
+
+C_rgb1=cmaperise(CV,gjet(250));
+C_rgb2=cmaperise(CV,viridis(250));
+C_rgb3=cmaperise(CV,spectral(250));
+
+%%
+
+cFigure; hold on;
+title('RGB data on vertices (interpolated)')
+h1= gedge(E,V ,C_rgb1,edgeThickness); h1.EdgeColor='interp';
+h2= gedge(E,V2,C_rgb2,edgeThickness); h2.EdgeColor='interp';
+h3= gedge(E,V3,C_rgb3,edgeThickness); h3.EdgeColor='interp';
+axisGeom; camlight headlight; 
+gdrawnow; 
+
+%%
+% 
+% <<gibbVerySmall.gif>>
+% 
+% _*GIBBON*_ 
+% <www.gibboncode.org>
+% 
+% _Kevin Mattheus Moerman_, <gibbon.toolbox@gmail.com>
+
+%% 
+% _*GIBBON footer text*_ 
+% 
+% License: <https://github.com/gibbonCode/GIBBON/blob/master/LICENSE>
+% 
+% GIBBON: The Geometry and Image-based Bioengineering add-On. A toolbox for
+% image segmentation, image-based modeling, meshing, and finite element
+% analysis.
+% 
+% Copyright (C) 2006-2022 Kevin Mattheus Moerman and the GIBBON contributors
+% 
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
---
+++ lib/gedge.m
@@ -0,0 +1,220 @@
+function [varargout]=gedge(varargin)
+
+% function [h]=gedge(E,V,CE,lineWidth,edgeAlpha)
+% ------------------------------------------------------------------------
+% This function is a short-hand version of the |patch| command. The inputs
+% for |gpatch| are the faces (F), the vertices (V), the color description
+% (C), the edge color description CE, the transparancy (A), and the edge
+% width (L). 
+% The color data descriptions C (or equivalently CE for edges) can be: 
+% 1) A string such as 'g' for green
+% 2) A triplet of RGD values e.g. [1 0 0] is blue
+% 3) A nx1 or a mx1 array of colormapped colors (where n=size(F,1) or
+% m=size(V,1)) 
+% 4) (simiarl to 3) A nx3 or a mx3 RGB color value array for the faces or
+% vertices respectively. 
+%
+%
+% Kevin Mattheus Moerman
+% gibbon.toolbox@gmail.com
+% 
+% 2017 
+% 2018/02/07 Added support for colormapped edges
+% 2019/07/03 Added handling of empty alpha data
+% 2021/05/12 Added "material dull" style lighting settings by default to
+% avoid white color reflectance distorting colormapped visualizations. 
+%------------------------------------------------------------------------
+
+switch nargin
+    case 1
+        error('Not enough input arguments, provide at least faces and vertices');
+    case 2
+        E=varargin{1};
+        V=varargin{2};
+        CE='k';        
+        lineWidth=1;
+        edgeAlpha=1;
+    case 3
+        E=varargin{1};
+        V=varargin{2};
+        CE=varargin{3};
+        lineWidth=1;
+        edgeAlpha=1;
+    case 4
+        E=varargin{1};
+        V=varargin{2};
+        CE=varargin{3};
+        lineWidth=varargin{4};
+        edgeAlpha=1;
+    case 5
+        E=varargin{1};
+        V=varargin{2};
+        CE=varargin{3};
+        lineWidth=varargin{4};
+        edgeAlpha=varargin{5}; 
+    otherwise
+        error('Wrong number of input arguments');
+end
+
+if isempty(CE)
+    CE='k';
+end
+
+if isempty(edgeAlpha)
+    edgeAlpha=1;
+end
+
+if isa(E,'cell') %Assume all entries are cells defining multiple patch data sets
+    for q=1:1:numel(E)
+        e=E{q};
+        
+        if isa(V,'cell')
+            v=V{q};
+        else
+            v=V;
+        end
+        
+        if isa(CE,'cell')
+            ce=CE{q};
+        else
+            ce=CE;
+        end
+        
+        if isa(edgeAlpha,'cell')
+            a=edgeAlpha{q};
+        else
+            a=edgeAlpha;
+        end
+        hp(q)=plotEdge(e,v,ce,lineWidth,a);
+    end
+else
+    hp=plotEdge(E,V,CE,lineWidth,edgeAlpha);
+end
+
+if nargout==1
+    varargout{1}=hp;
+end
+
+end
+
+%%
+function hp=plotEdge(E,V,CE,lineWidth,edgeAlpha)
+
+%Avoid reflectance (similar to material dull)
+argInPatch.DiffuseStrength=0.6;
+argInPatch.AmbientStrength=0.4;
+argInPatch.SpecularExponent=10;
+argInPatch.SpecularStrength=0;
+argInPatch.SpecularColorReflectance=0;
+
+if ischar(CE) %Plain single color
+    argInPatch.EdgeColor=CE;    
+    if strcmp(CE,'kw')
+        argInPatch.EdgeColor=grayColor(0.5);
+    end
+    if strcmp(CE,'rw')
+        argInPatch.EdgeColor=[1 0.5 0.5];
+    end
+    if strcmp(CE,'gw')
+        argInPatch.EdgeColor=[0.5 1 0.5];
+    end
+    if strcmp(CE,'bw')
+        argInPatch.EdgeColor=[0.5 0.5 1];
+    end
+    if strcmp(CE,'yw')
+        argInPatch.EdgeColor=[1 1 0.5];
+    end
+    if strcmp(CE,'cw')
+        argInPatch.EdgeColor=[0.5 1 1];
+    end
+    if strcmp(CE,'mw')
+        argInPatch.EdgeColor=[1 0.5 1];
+    end    
+elseif size(CE,2)==1    
+    if size(CE,1)>1
+        if size(CE,1)==size(E,1)
+            [E,V]=patchDetach(E,V);
+            argInPatch.Vertices=V;
+            [CE]=faceToVertexMeasure(E,V,CE);   
+            argInPatch.EdgeColor='flat';
+            argInPatch.CData=double(CE);
+        end
+        if size(CE,1)==size(V,1)
+            argInPatch.EdgeColor='flat';
+            argInPatch.CData=double(CE);
+        end
+    else
+        argInPatch.EdgeColor='flat';
+        argInPatch.CData=double(CE)*ones(size(V,1),1);
+    end
+
+elseif size(CE,2)==3
+
+    if size(CE,1)>1
+        if size(CE,1)==size(E,1)
+            [E,V]=patchDetach(E,V);
+            argInPatch.Vertices=V;
+            [CE]=faceToVertexMeasure(E,V,CE);            
+        end        
+        argInPatch.EdgeColor='flat';
+        argInPatch.FaceVertexCData=double(CE);
+    elseif size(CE,1)==1
+        argInPatch.EdgeColor=double(CE);        
+    end
+
+else
+    error('Invalid edge color data input');
+end
+
+if numel(edgeAlpha)==1 %Plain single alpha
+    argInPatch.edgeAlpha=double(edgeAlpha);
+elseif size(edgeAlpha,1)>1 %Alpha mapping
+
+    if size(edgeAlpha,1)==size(E,1)
+        [E,V]=patchDetach(E,V);
+        [edgeAlpha]=faceToVertexMeasure(E,V,edgeAlpha);
+        argInPatch.EdgeAlpha='flat';
+        argInPatch.FaceVertexAlphaData=double(edgeAlpha);    
+    end
+    argInPatch.EdgeColor='flat';
+    argInPatch.FaceVertexCData=double(edgeAlpha);
+else
+    error('Invalid alpha data input');
+end
+
+if ~isempty(lineWidth)
+    argInPatch.LineWidth=lineWidth;
+end
+
+argInPatch.Faces=E;
+argInPatch.Vertices=V;
+%argInPatch.EdgeColor=CE;
+argInPatch.FaceColor='none';
+
+hp=patch(argInPatch);
+
+end
+ 
+%% 
+% _*GIBBON footer text*_ 
+% 
+% License: <https://github.com/gibbonCode/GIBBON/blob/master/LICENSE>
+% 
+% GIBBON: The Geometry and Image-based Bioengineering add-On. A toolbox for
+% image segmentation, image-based modeling, meshing, and finite element
+% analysis.
+% 
+% Copyright (C) 2006-2022 Kevin Mattheus Moerman and the GIBBON contributors
+% 
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
