52bfa643d06f0af5508dc1af618e663ec1c3769d
Kevin-Mattheus-Moerman <kevin.moerman@gmail.com>
2023-05-29 16:31:05 +0100

    Added prin2effective for von mises stress and effective strain computation

---
+++ lib/prin2effective.m
@@ -0,0 +1,37 @@
+function D_eff=prin2effective(varargin)
+
+% function D_eff=prin2effective(D1,D2,D3,typeFlag)
+% ------------------------------------------------------------------------
+% Computes the effective/Von Mises stress or strain based on the input
+% principal components. 
+% 
+% 2023/05/11: Created
+% ------------------------------------------------------------------------
+
+%% Parse input
+
+switch nargin
+    case 2
+        D=varargin{1};
+        typeFlag=varargin{2};
+        D1=D(:,1,:);
+        D2=D(:,2,:);
+        D3=D(:,3,:);        
+    case 4
+        D1=varargin{1};
+        D2=varargin{2};
+        D3=varargin{3};  
+        typeFlag=varargin{4};
+    otherwise
+        error('Wrong number of input arguments.');
+end
+
+%%
+
+%Compute Von Mises / effective metric
+switch typeFlag
+    case 'stress' %Von Mises Stress
+        D_eff =         sqrt( ((D1-D2).^2 + (D2-D3).^2 + (D1-D3).^2)./2 );
+    case 'strain' %Effective strain
+        D_eff = (1/3).* sqrt( ((D1-D2).^2 + (D2-D3).^2 + (D1-D3).^2).*2 );
+end
