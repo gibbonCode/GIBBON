e7f9ad14af14c5dc67af06f70011385e78545103
Kevin-Mattheus-Moerman <kevin.moerman@gmail.com>
2023-11-07 10:53:18 +0000

    Updated documentation for logicRemoveInterior

--- docs/HELP_logicRemoveInterior.m
+++ docs/HELP_logicRemoveInterior.m
@@ -8,9 +8,107 @@ clear; close all; clc;
 % |[Lb]=logicRemoveInterior(L);|
 
 %% Description 
-% UNDOCUMENTED 
+% This function removes the interior entries for the input logic L.
+% Interior entries are those fully surrounded by neighbours (e.g. in 3D all
+% entries which are attached to a top, bottom, left, right, front, and a
+% back neighbour).
+% Vectors and n-dimensional arrays are supported. 
+
 %% Examples 
-% 
+%
+
+%% Example 1: 1D logical treatment
+
+L = false(12,1);
+L(3:10)=1;
+
+%%
+
+[Lb]=logicRemoveInterior(L);
+
+[Lb]=logicRemoveInterior(L);
+
+%%
+
+cFigure; 
+subplot(1,2,1);
+imagesc(L); 
+axis tight; axis equal; colormap gray; 
+
+subplot(1,2,2);
+imagesc(Lb); 
+axis tight; axis equal; colormap gray; 
+drawnow;
+cFigure; 
+subplot(1,2,1);
+imagesc(L); 
+axis tight; axis equal; colormap gray; 
+
+subplot(1,2,2);
+imagesc(Lb); 
+axis tight; axis equal; colormap gray; 
+drawnow;
+
+%% Example 2: 2D logical treatment
+
+r=2;
+n=25;
+x=linspace(-1.5*r,1.5*r,n);
+[X,Y]=ndgrid(x);
+R = sqrt(X.^2+Y.^2);
+
+L = R<=r; 
+
+%%
+
+[Lb]=logicRemoveInterior(L);
+
+%%
+
+cFigure; 
+subplot(1,2,1);
+imagesc(L); 
+axis tight; axis equal; colormap gray; 
+
+subplot(1,2,2);
+imagesc(Lb); 
+axis tight; axis equal; colormap gray; 
+drawnow;
+
+%% Example 3: 3D logical treatment
+
+r=2;
+n=25;
+x=linspace(-1.5*r,1.5*r,n);
+[X,Y,Z]=ndgrid(x);
+R = sqrt(X.^2+Y.^2+Z.^2);
+
+L = R<=r; 
+
+%%
+
+[Lb]=logicRemoveInterior(L);
+
+%Force half to be zero so we can look inside 
+L(:,:,ceil(n/2):end)=0; 
+Lb(:,:,ceil(n/2):end)=0; 
+%%
+
+[F,V,C]=im2patch(L,L);
+[Fb,Vb,Cb]=im2patch(Lb,L);
+
+cFigure; 
+subplot(1,2,1);
+gpatch(F,V,C); 
+axisGeom; colormap gray; clim([0 1]);
+camlight headlight; 
+
+subplot(1,2,2);
+gpatch(Fb,Vb,Cb); 
+axisGeom; colormap gray; clim([0 1]);
+camlight headlight; 
+drawnow;
+
 %%
 % 
 % <<gibbVerySmall.gif>>
